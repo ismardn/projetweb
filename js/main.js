580c4c97074cea08e89e75165d0a7ce10c8ce74b
ismardn <ismael.radouane@efrei.net>
2025-05-16 21:13:11 +0200

    Avancement dans la page d'accueil : ajout des vidéos et du texte présentant le site + ajout css

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function playVideo(card) {
+    const video = card.querySelector("video");
+    video.play();
+}
+
+function pauseVideo(card) {
+    const video = card.querySelector("video");
+    video.pause();
+}
